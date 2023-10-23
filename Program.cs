6dbdaec03498230d01f242eeb6df44674dd3020b
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-10-23 15:33:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Inlämningsuppgifter4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange sidlängd för triangeln: ");
+            int sidlängd = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= sidlängd; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
